b951651f4feb1f1f45155b2ab3be2220dc48724d
Karol Majta <karolmajta@gmail.com>
2014-01-02 22:15:02 +0100

    Finished introduction

--- 0001_introduction.js
+++ 0001_introduction.js
@@ -23,7 +23,7 @@ assertEquals(__, (function () { return "Hello World!"; })() );
 /*
  * Przed chwilą uruchomiłeś swój pierwszy *automatyczny test*. Pisanie testów
  * jest świetne, ale nie jest głównym tematem naszego kursu, dlatego wszystkie
- * testy z jakimi się zetkniesz w tym skrypcie już napisałem za Ciebie. Twoim
+ * testy z jakimi się zetkniesz w tym skrypcie już napisałem za Ciebie.Twoim
  * zadaniem będzie uzupełnić formularz tak, aby po podstawieniu jego zawartości
  * do testu w miejsce podwójnego podkreślnika (__) program wykonał się poprawnie.
  * Mówiąc językiem testów, chodzi o to, aby wszystkie asercje "przeszły".
@@ -36,9 +36,8 @@ assertEquals(__, (function () { return "Hello World!"; })() );
  *
  * Kolor komunikatu ma znaczenie - jeśli jest pomarańczowy, oznacza to, że któraś
  * z asercji nie przeszła. Treść komunikatu powie Ci co było tego powodem. Jeśli
- * komunikat jest czerwony, to po podstawieniu wpisany przez Ciebie tekst po
- * podstawieniu w miejsce podwójnego podkreślnika nie tworzy poprawnego kodu
- * JavaScript. Najlepiej sprawdź wówczas brakujące cudzysłowie, nawiasy i śrendiki.
+ * komunikat jest czerwony, to wpisany przez Ciebie tekst po podstawieniu w miejsce
+ * podwójnego podkreślnika nie tworzy poprawnego kodu JavaScript.
  *
  * Przedstawiona poniżej asercja `assert` jest bardzo prosta. Sprawdza czy jej
  * pierwszy argument jest "prawdziwy" (specjalnie użyłem cudzysłowiu, dlaczego
@@ -49,3 +48,64 @@ assertEquals(__, (function () { return "Hello World!"; })() );
  */
 
 assert(__, "Nie mówisz mi całej prawdy!");
+
+// assert-equals|assertEquals
+
+/* Z asercją `assertEquals` miałeś do czynienia przed chwilą. Sprawdza ona, czy
+ * obydwa jej argumenty są sobie równe. Warto zauważyć, że wykorzystuje ona do tego
+ * operator `==`. Nie przejmuj się, jeśli nie wiesz jeszcze co z tego wynika. Spróbuj
+ * wpisać w poniższą asercję coś co jest fałszywe. Możesz popuścić wodzę fantazji,
+ * bo w JavaScript nie tylko `false` jest równe `false`.
+ */
+
+assertEquals(__, false);
+
+// assert-not-equals|assertNotEquals
+
+/*
+ * `AssertNotEquals` to przeciwieństwo `assertEquals`. Sprawdza, czy dwa elementy są
+ * różne od siebie, a robi to z pomocą operatora `!=`. Specjalnie jako jej drugi argument
+ * podałem `true` - powinna zadziałać dla wartości z powyższego przykładu. Prawda?
+ */
+
+assertNotEquals(__, true);
+
+// assert-strict-equals|assertStrictEquals
+
+/*
+ * Zauważyłeś już pewnie, że pojęcie "równości" jest w JS dość... nieortodoksyjne.
+ * Na szczęście istnieją sposoby, aby sprawdzić, że dwie zmienne są "na prawdę równe".
+ * Służy do tego operator `===`. Asercja `assertStrictEquals` używa tego właśnie
+ * operatora, aby stwierdzić tzw. "dokładną równość" (strict equality) pomiędzy
+ * dwiema zmiennymi. Sprawdź, czy wartość, którą wykorzystałeś powyżej jest
+ * "dokładnie równa" logicznej wartości `false`.
+ */
+
+assertStrictEquals(__, false);
+
+// assert-not-strict-equals|assertNotStrictEquals
+
+/*
+ * Podobnie jak `assertEquals`, również `assertStrictEquals` posiada swoje
+ * przeciwieństwo. Asercja `assertNotStrictEquals` sprawdza, czy dwie zmienne
+ * są "dokładnie nierówne" (tak, wiem jak to brzmi...). Spróbuj pobawić się tą
+ * asercją wykorzystująć liczbę `1` i string `"1"`.
+ */
+
+assertNotStrictEquals(__, "1");
+
+// co-wiedziec|Co powinieneś wiedzieć?
+
+/*
+ * Nie przejmuj się, jeśli nie zrozumiałeś wszystkiego z tego rozdziału,
+ * będziemy dokładnie omawiać jak w JS działają zmienne, operatory, funkcje
+ * i inne rzeczy. Chciałbym natomiast, żebyś potrafił sprawnie posługiwać
+ * się tą aplikacją, to znaczy odróżniał błędy asercji od błędów składni,
+ * oraz wiedział jak działają poszczególne asercje.
+ *
+ * Jeśli masz ochotę na nieco więcej, spróbuj rozwiązać przykład poniżej.
+ */
+
+assert(__, "Rzeczy nie zawsze są tym czym się wydają...");
+assertEquals(__, 0);
+assertStrictEquals(10 + __, "100");
